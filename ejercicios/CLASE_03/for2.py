a4531edc1985e9d41334a85b16a8f8c502ffc97d
framogaza <framogaza@gmail.com>
2021-04-30 17:50:51 -0500

    Ejercicios en clase
    PYTHON_BASICS

---
+++ ejercicios/CLASE_03/for2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 29 19:04:12 2019
+
+@author: CEC
+"""
+
+for A in range(10, 1, -1):
+    print(A)
+# 10
+# 8
+# 6
+# 4
+# 2
---
+++ ejercicios/CLASE_03/forrange.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 29 09:18:24 2019
+
+@author: Juan Carlos
+"""
+
+for i in "AMIGO":
+    print("Dame una", {i})
+print("¡AMIGO!")
